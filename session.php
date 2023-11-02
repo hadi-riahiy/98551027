b6f898fc6d6a7f35e3fd5d82c2e72b8f0bf64b4a
hadi-riahiy <13hadiriahi79@gmail.com>
2023-11-02 21:37:11 +0330

    first commit

---
+++ session.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>اولین تمرين برنامه نویسی وب</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    </head>
+    <body dir="rtl">
+        <h1 style="font-size: 175% ; color:red">برنامه ترم اول سال تحصیلی 401-402 :</h1>
+        <div class="row col-md-6 col-sm-12" style="margin-right:auto;margin-left:auto;">
+            <table class="table table-bordered table-striped">
+                <thead class="table-dark">
+                <tr>
+                    <th>روز كلاس / ساعت </th>
+                    <th>8-10</th>
+                    <th>10-12</th>
+                    <th>13:30-15:30</th>
+                    <th>15:30-17:30</th>
+                </tr>
+            </thead>
+                <tr>
+                    <td>شنبه</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                </tr>
+                <tr>
+                    <td>یک شنبه</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                </tr>
+                <tr>
+                    <td>دو شنبه</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                </tr>
+                <tr>
+                    <td>سه شنبه</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                </tr>
+                <tr>
+                    <td>چهارشنبه</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
